83c4f0bb9d2aec840d7ecd1076be58a2fdb9fedc
rouzwelt <rouzweltt@gmail.com>
2023-01-27 23:43:35 +0000

    update

--- bin/a.js
+++ bin/a.js
@@ -1,15 +1,2 @@
-
-const main = async () => {
-  console.log("heyyy")
-};
-
-main()
-  .then(() => {
-    const exit = process.exit;
-    exit(0);
-  })
-  .catch((error) => {
-    console.error(error);
-    const exit = process.exit;
-    exit(1);
-  });
+#!/usr/bin/env ts-node
+console.log("hey")
