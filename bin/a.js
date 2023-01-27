110c23084e613bf6e772db99872c987473dd384d
rouzwelt <rouzweltt@gmail.com>
2023-01-27 23:10:45 +0000

    update

---
+++ bin/a.js
@@ -0,0 +1,15 @@
+
+const main = async () => {
+  console.log("heyyy")
+};
+
+main()
+  .then(() => {
+    const exit = process.exit;
+    exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    const exit = process.exit;
+    exit(1);
+  });
